bf1b10ae12d7a802ddf3a8c06b12366bc61aadcc
Gauravkumarchaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-07-30 23:19:43 +0530

    Basics of R

---
+++ R_basics.R
@@ -0,0 +1,14 @@
+### base Packages ###
+library()
+library(scatterplot3d)
+install.packages("scatterplot3d")
+
+#### Directory #####
+
+getwd()
+setwd("C:\\Users\\gaura\\OneDrive\\Desktop\\Data Science with r")
+getwd()
+
+help(getwd)
+ list.files()
+#normalizePath("Data Science with r")     
