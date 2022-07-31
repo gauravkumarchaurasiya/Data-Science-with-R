ce3fa363d003969288b67b07fa46249a585d9d69
Gauravkumarchaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-07-31 09:54:22 +0530

    Vectors in r

---
+++ R_Vectors.R
@@ -0,0 +1,24 @@
+##### VECTORS #####
+
+a= c( 3,5,6,6) # initializing a vector
+class(a)
+length(a)
+b=c(2:5)
+b
+c=a+b
+c
+c[5]= 44 # vectors are mutable
+
+c[1:4] # numeric slicing
+
+?c[1,5]
+c[-2]
+
+a
+c
+print(c+a)
+
+v=c(3,6,2,"gaurav")
+typeof(v)
+class(v)
+v
